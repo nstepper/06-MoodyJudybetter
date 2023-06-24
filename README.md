ecb8228c52db1a173277dec840424bbc8393b6fe
Nathan Stepper <nstepper@uw.edu>
2023-06-24 00:20:03 -0700

    tryign to fix the jank

--- README.md
+++ README.md
@@ -1,1 +1,48 @@
-# 06-MoodyJudybetter
+# Weather Dashboard
+
+## Description 
+
+Weather Dashboard is a dynamic web application that provides users with real-time weather data, including current conditions and a five-day forecast, for any city they search. This application is especially handy for travelers who need to plan their trips based on weather conditions. 
+
+## Features 
+
+- Search for a city and retrieve its current weather conditions (temperature, humidity, and wind speed).
+- View a 5-day weather forecast for the searched city.
+- An intuitive UI displaying an icon representation of weather conditions.
+- Previous city searches are saved in the search history for quick access. 
+
+## Technologies Used
+
+- HTML5
+- CSS3
+- JavaScript
+- jQuery
+- OpenWeatherMap API
+
+## Installation
+
+No installation is needed. Simply open the `index.html` file in your preferred web browser.
+
+## Usage
+
+1. Locate the search box in the dashboard.
+2. Enter a city name into the search box and press "Search".
+3. The current weather conditions for that city will be displayed, including the city name, the date, an icon representation of weather conditions, the temperature, the humidity, and the wind speed.
+4. Additionally, a 5-day forecast for that city will be presented, which includes the date, an icon representation of weather conditions, the temperature, the wind speed, and the humidity for each day.
+5. The searched city is added to the search history. Clicking on a city in the search history will again present you with current and future conditions for that city.
+
+## Demo
+
+![demo](assets/06mood.gif)
+
+## Deployment Link
+
+githubpages
+
+## License
+
+MIT
+
+## Contact
+
+- GitHub: https://github.com/nstepper
--- assets/script.js
+++ assets/script.js
@@ -1,122 +1,122 @@
-$(document).ready(function() {
-  const apiKey = '86eb291f600c49db2d915a2be71795e2';
-  const cityInput = document.querySelector('#city-input');
-  const currentWeather = document.querySelector('#current-weather');
-  const forecast = document.querySelector('#future-weather'); // Changed from #forecast to #future-weather
-  let searchHistory = [];
-  searchHistory = JSON.parse(localStorage.getItem('searchHistory')) || [];
+$(document).ready(function () {
+    const apiKey = '86eb291f600c49db2d915a2be71795e2';
+    const cityInput = document.querySelector('#city-input');
+    const currentWeather = document.querySelector('#current-weather');
+    const forecast = document.querySelector('#future-weather'); // Changed from #forecast to #future-weather
+    let searchHistory = [];
+    searchHistory = JSON.parse(localStorage.getItem('searchHistory')) || [];
 
-  // Search for city weather data and update UI
-  function searchCity(city) {
-      if(city == null) {
-          return;
-      }
-      // API call to get current and forecast weather data
-      fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`)
-          .then(response => response.json())
-          .then(data => {
-              // Create current weather card
-              const currentWeatherCard = createCurrentWeatherCard(data);
-              currentWeather.innerHTML = '';
-              currentWeather.appendChild(currentWeatherCard);
-  
-              // Add city to search history
-              if (!searchHistory.includes(city)) {
-                  searchHistory.push(city);
-                  localStorage.setItem('searchHistory', JSON.stringify(searchHistory));
-                  createSearchHistory(searchHistory);
-              }
-  
-              // API call to get 5-day forecast weather data
-              fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${city}&units=metric&appid=${apiKey}`)
-                  .then(response => response.json())
-                  .then(data => {
-                      // Create forecast cards
-                      const forecastCards = createForecastCards(data);
-                      forecast.innerHTML = '';
-                      forecast.appendChild(forecastCards);
-                  })
-                  .catch(error => {
-                      console.error(error);
-                  });
-          })
-          .catch(error => {
-              console.error(error);
-              alert('City not found. Please try again.');
-          });
-  }
+    // Search for city weather data and update UI
+    function searchCity(city) {
+        if (city == null) {
+            return;
+        }
+        // API call to get current and forecast weather data
+        fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`)
+            .then(response => response.json())
+            .then(data => {
+                // Create current weather card
+                const currentWeatherCard = createCurrentWeatherCard(data);
+                currentWeather.innerHTML = '';
+                currentWeather.appendChild(currentWeatherCard);
 
-  // Create current weather card
-  function createCurrentWeatherCard(data) {
-      const currentWeatherCard = document.createElement('section');
-      currentWeatherCard.classList.add('weather-card', 'current-weather');
+                // Add city to search history
+                if (!searchHistory.includes(city)) {
+                    searchHistory.push(city);
+                    localStorage.setItem('searchHistory', JSON.stringify(searchHistory));
+                    createSearchHistory(searchHistory);
+                }
 
-      const city = document.createElement('h2');
-      city.textContent = data.name;
-      currentWeatherCard.appendChild(city);
+                // API call to get 5-day forecast weather data
+                fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${city}&units=metric&appid=${apiKey}`)
+                    .then(response => response.json())
+                    .then(data => {
+                        // Create forecast cards
+                        const forecastCards = createForecastCards(data);
+                        forecast.innerHTML = '';
+                        forecast.appendChild(forecastCards);
+                    })
+                    .catch(error => {
+                        console.error(error);
+                    });
+            })
+            .catch(error => {
+                console.error(error);
+                alert('City not found. Please try again.');
+            });
+    }
 
-      const date = document.createElement('p');
-      date.textContent = new Date().toLocaleDateString();
-      currentWeatherCard.appendChild(date);
+    // Create current weather card
+    function createCurrentWeatherCard(data) {
+        const currentWeatherCard = document.createElement('section');
+        currentWeatherCard.classList.add('weather-card', 'current-weather');
 
-      const icon = document.createElement('img');
-      icon.src = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`; // use backticks
-      icon.alt = data.weather[0].description;
-      currentWeatherCard.appendChild(icon);
+        const city = document.createElement('h2');
+        city.textContent = data.name;
+        currentWeatherCard.appendChild(city);
 
-      const temperature = document.createElement('p');
-      temperature.innerHTML = `Temperature: ${data.main.temp}&deg;C`; // use backticks
-      currentWeatherCard.appendChild(temperature);
+        const date = document.createElement('p');
+        date.textContent = new Date().toLocaleDateString();
+        currentWeatherCard.appendChild(date);
 
-      const humidity = document.createElement('p');
-      humidity.textContent = `Humidity: ${data.main.humidity}%`; // use backticks
-      currentWeatherCard.appendChild(humidity);
+        const icon = document.createElement('img');
+        icon.src = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`; // use backticks
+        icon.alt = data.weather[0].description;
+        currentWeatherCard.appendChild(icon);
 
-      const windSpeed = document.createElement('p');
-      windSpeed.textContent = `Wind Speed: ${data.wind.speed} m/s`; // use backticks
-      currentWeatherCard.appendChild(windSpeed);
+        const temperature = document.createElement('p');
+        temperature.innerHTML = `Temperature: ${data.main.temp}&deg;C`; // use backticks
+        currentWeatherCard.appendChild(temperature);
 
-      return currentWeatherCard;
-  }
+        const humidity = document.createElement('p');
+        humidity.textContent = `Humidity: ${data.main.humidity}%`; // use backticks
+        currentWeatherCard.appendChild(humidity);
 
-  // Create forecast cards
-  function createForecastCards(data) {
-      const forecastCards = document.createElement('div');
-      forecastCards.classList.add('forecast-cards');
+        const windSpeed = document.createElement('p');
+        windSpeed.textContent = `Wind Speed: ${data.wind.speed} m/s`; // use backticks
+        currentWeatherCard.appendChild(windSpeed);
 
-      for (let i = 0; i < data.list.length; i += 8) {
-          const forecastCard = document.createElement('section');
-          forecastCard.classList.add('weather-card', 'forecast');
-  
-          const date = document.createElement('h2');
-          date.textContent = new Date(data.list[i].dt * 1000).toLocaleDateString();
-          forecastCard.appendChild(date);
-  
-          const icon = document.createElement('img');
-          icon.src = `https://openweathermap.org/img/w/${data.list[i].weather[0].icon}.png`; // use backticks
-          icon.alt = data.list[i].weather[0].description;
-          forecastCard.appendChild(icon);
-  
-          const temperature = document.createElement('p');
-          temperature.innerHTML = `Temperature: ${data.list[i].main.temp}°C`; // use backticks
-          forecastCard.appendChild(temperature); // this line was missing in your original code
-  
-          forecastCards.appendChild(forecastCard);
-      }
-      return forecastCards;
-  }
+        return currentWeatherCard;
+    }
 
-  // click event for the search button
-  $("form").on("submit", function (event) {
-      event.preventDefault(); // prevent form submission and page refresh
-      // send user input
-      searchCity(cityInput.value);
-  });
+    // Create forecast cards
+    function createForecastCards(data) {
+        const forecastCards = document.createElement('div');
+        forecastCards.classList.add('forecast-cards');
 
-  function createSearchHistory(searchTerm) {
-      var searchDate = new Date().toLocaleDateString();
-      var listItem = $("<li>").text(searchTerm + " - " + searchDate);
-      searchHistory.unshift(listItem);
-      $('#search-history').prepend(listItem); // added this line to display the search history on the page
-  }
+        for (let i = 0; i < data.list.length; i += 8) {
+            const forecastCard = document.createElement('section');
+            forecastCard.classList.add('weather-card', 'forecast');
+
+            const date = document.createElement('h2');
+            date.textContent = new Date(data.list[i].dt * 1000).toLocaleDateString();
+            forecastCard.appendChild(date);
+
+            const icon = document.createElement('img');
+            icon.src = `https://openweathermap.org/img/w/${data.list[i].weather[0].icon}.png`; // use backticks
+            icon.alt = data.list[i].weather[0].description;
+            forecastCard.appendChild(icon);
+
+            const temperature = document.createElement('p');
+            temperature.innerHTML = `Temperature: ${data.list[i].main.temp}°C`; // use backticks
+            forecastCard.appendChild(temperature); // this line was missing in your original code
+
+            forecastCards.appendChild(forecastCard);
+        }
+        return forecastCards;
+    }
+
+    // click event for the search button
+    $("form").on("submit", function (event) {
+        event.preventDefault(); // prevent form submission and page refresh
+        // send user input
+        searchCity(cityInput.value);
+    });
+
+    function createSearchHistory(searchTerm) {
+        var searchDate = new Date().toLocaleDateString();
+        var listItem = $("<li>").text(searchTerm + " - " + searchDate);
+        searchHistory.unshift(listItem);
+        $('#search-history').prepend(listItem); // added this line to display the search history on the page
+    }
 });
